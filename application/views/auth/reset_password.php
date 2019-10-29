6a63a1d5bd7125a2a1aadeeed48153f2eda4027f
jual ahmed <md.jual.ah@gmail.com>
2019-10-29 22:56:55 +0600

    sdfs

--- application/views/auth/reset_password.php
+++ application/views/auth/reset_password.php
@@ -12,13 +12,13 @@
 
         <p>
           <?php echo lang('reset_password_new_password_confirm_label', 'new_password_confirm');?> <br />
-          <?php echo form_input($new_password_confirm, array('class' =>'text-lowercase form-control','placeholder'=>"Enter new Passsword"));?>
+          <?php echo form_input($new_password_confirm);?>
         </p>
 
-        <?php echo form_input($user_id,array('class' =>'text-lowercase form-control','placeholder'=>"Confirm new Passsword"));?>
+        <?php echo form_input($user_id);?>
         <?php echo form_hidden($csrf); ?>
 
-        <p><?php echo form_submit('submit', "reset_password",array('class' =>'btn btn-success text-white py-2 px-5 roundeddd'));?></p>
+        <p><?php echo form_submit('submit', "reset_password");?></p>
 
       <?php echo form_close();?>
   </div>
