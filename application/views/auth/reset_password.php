bd02992e859eddb1b7e1c5d6e78d421282e8c3da
jual ahmed <md.jual.ah@gmail.com>
2019-10-29 23:01:20 +0600

    sdfsdf

--- application/views/auth/reset_password.php
+++ application/views/auth/reset_password.php
@@ -1,5 +1,5 @@
 <div class="container py-4 my-4 text-center">
-  <div class="w-50 m-auto box-shadow py-3">
+  <div class="w-50 m-auto box-shadow p-5">
       <h1><?php echo lang('reset_password_heading');?></h1>
       <div id="infoMessage"><?php echo $message;?></div>
 
@@ -7,12 +7,12 @@
 
         <p>
           <label for="new_password"><?php echo sprintf(lang('reset_password_new_password_label'), $min_password_length);?></label> <br />
-         <input type="password" name="new" value="" class="form-control text-none" id="new" pattern="^.{8}.*$">
+         <input type="password" placeholder="New password" name="new" value="" class="form-control text-none" id="new" pattern="^.{8}.*$">
         </p>
 
         <p>
           <?php echo lang('reset_password_new_password_confirm_label', 'new_password_confirm');?> <br />
-         <input type="password" name="new_confirm" value="" id="new_confirm" class="form-control text-none" pattern="^.{8}.*$">
+         <input type="password"  placeholder="Confirm New password" name="new_confirm" value="" id="new_confirm" class="form-control text-none" pattern="^.{8}.*$">
         </p>
 
         <?php echo form_input($user_id);?>
