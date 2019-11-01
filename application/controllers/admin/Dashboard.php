301a095931f949cb86897bf8a21672952357cb05
jual ahmed <md.jual.ah@gmail.com>
2019-11-01 13:17:09 +0600

    make admin countable

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -2,35 +2,31 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends Admin_Controller {
+  public function __construct()
+  {
+      parent::__construct();
 
-    public function __construct()
-    {
-        parent::__construct();
+      /* Load :: Common */
+      $this->load->helper('number');
+      $this->load->model('admin/dashboard_model');
+  }
 
-        /* Load :: Common */
-        $this->load->helper('number');
-        $this->load->model('admin/dashboard_model');
-    }
-
-
-	public function index()
-	{
+  public function index()
+  {
         if ( ! $this->ion_auth->logged_in() OR ! $this->ion_auth->is_admin())
         {
             redirect('auth/login', 'refresh');
         }
         else
         {
-            /* Title Page */
             $this->page_title->push(lang('menu_dashboard'));
             $this->data['pagetitle'] = $this->page_title->show();
-
-            /* Breadcrumbs */
+            
             $this->data['breadcrumb'] = $this->breadcrumbs->show();
-
-            /* Data */
             $this->data['count_users']       = $this->dashboard_model->get_count_record('users');
-            $this->data['count_groups']      = $this->dashboard_model->get_count_record('groups');
+            $this->data['testimonials']      = $this->dashboard_model->get_count_record('testimonials');
+            $this->data['exchanges']      = $this->dashboard_model->get_count_record('exchanges');
+            $this->data['gateways']      = $this->dashboard_model->get_count_record('gateways');
             $this->data['disk_totalspace']   = $this->dashboard_model->disk_totalspace(DIRECTORY_SEPARATOR);
             $this->data['disk_freespace']    = $this->dashboard_model->disk_freespace(DIRECTORY_SEPARATOR);
             $this->data['disk_usespace']     = $this->data['disk_totalspace'] - $this->data['disk_freespace'];
@@ -39,13 +35,16 @@ class Dashboard extends Admin_Controller {
             $this->data['memory_peak_usage'] = $this->dashboard_model->memory_peak_usage(TRUE);
             $this->data['memory_usepercent'] = $this->dashboard_model->memory_usepercent(TRUE, FALSE);
 
+            $this->db->select('users.*,gateways.*,currency.*,exchanges.*,exchanges.status as statuss');
+            $this->db->join('users', 'users.id = exchanges.user_id');
+            $this->db->join('gateways', 'gateways.id = exchanges.gateway_send');
+            $this->db->join('currency', 'gateways.currency = currency.currency_id');
+            $this->db->order_by("exchanges.id", "desc");
+            $this->db->where('exchanges.status',1);
+            $this->data['results'] = $this->db->get("exchanges")->result();
 
-            /* TEST */
             $this->data['url_exist']    = is_url_exist('http://www.domprojects.com');
-
-
-            /* Load Template */
             $this->template->admin_render('admin/dashboard/index', $this->data);
         }
-	}
+  }
 }
